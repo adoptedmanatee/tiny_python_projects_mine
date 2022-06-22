dbc1959d5fd4eb17289a88d59328ee58398fe388
Wesley Robertson <adoptedmanatee@gmail.com>
2022-06-22 15:15:13 +0000

    Completed some items

---
+++ 01_hello/hello.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3 
+# Shebang tells the kernel which language to use without specifying 'python3 myscript'. Just use ./myscript 
+# Important to use 'env' here because it will load the computers environment variables and find the appropriate binary (which ptyhon3)
+print('Hello, World!')
