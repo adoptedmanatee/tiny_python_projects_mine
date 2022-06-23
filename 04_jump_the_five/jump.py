48059cc6b70dcacfe592aa243db39a7a82bbeba7
Wesley Robertson <adoptedmanatee@gmail.com>
2022-06-23 10:01:42 -0400

    Update jump.py
    Changed logic for and variable descriptions for ease

--- 04_jump_the_five/jump.py
+++ 04_jump_the_five/jump.py
@@ -13,24 +13,18 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Input phone number and perform encryption',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        description="Input phone number and perform encryption",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-    parser.add_argument('positional',
-                        metavar='phone_number',
-                        help='Phone number to be encrypted')
+    parser.add_argument(
+        "positional",
+        metavar="phone_number",
+        help="Phone number to be encrypted",
+        nargs="+",
+    )
 
-    # parser.add_argument('-a',
-    #                     '--arg',
-    #                     help='A named string argument',
-    #                     metavar='str',
-    #                     type=str,
-    #                     default='')
-
-    parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
+    parser.add_argument("-o", "--on", help="A boolean flag", action="store_true")
 
     return parser.parse_args()
 
@@ -40,15 +34,37 @@ def main():
 
     args = get_args()
     pos_arg = args.positional
-    encrypteR = {'1':'9', '2':'8', '3':'7', '4':'6', '5':'0', '6':'4', '7':'3', '8':'2', '9':'1', '0':'5'}
-    encrypteD = []
-    for i in str(pos_arg):
-      if i in encrypteR:
-        
-        encrypteD.append(encrypteR[i])
-      # print(f'{encrypter[i]}')
-    print(''.join(encrypteD))
+    encrypteR = {
+        "1": "9",
+        "2": "8",
+        "3": "7",
+        "4": "6",
+        "5": "0",
+        "6": "4",
+        "7": "3",
+        "8": "2",
+        "9": "1",
+        "0": "5",
+    }
+
+    phrase = pos_arg
+    new_phrase = []
+
+    for word in phrase:
+        new_word_or_digits = []
+        list_of_individual_letters_or_digits = list(word)
+
+        for character in list_of_individual_letters_or_digits:
+            if character in encrypteR:
+                character = encrypteR[character]
+
+            new_word_or_digits.append(character)
+
+        new_phrase.append("".join(new_word_or_digits))
+
+    print(" ".join(new_phrase))
+
 
 # --------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
